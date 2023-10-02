490ca64005e15fe1e47a6c1bf62dfff5046d7154
dgold01 <d.l.gold@hotmail.com>
2023-10-02 11:14:52 +0100

    Test: added more tests for saving score to database

--- Backend/tests/handler.test.js
+++ Backend/tests/handler.test.js
@@ -1,11 +1,11 @@
-const axiosService = require('../axiosService')
+import { fetchData } from '../axiosService';
 const { fetchWordList, fetchScores, storeHighScore } = require('../handler');
 const AWS = require('aws-sdk-mock');
 
-jest.mock('./axiosService', () => ({
-    fetchData: jest.fn(), 
-  }));
-  
+jest.mock('../axiosService', () => ({
+    fetchData: jest.fn(),
+}));
+
 
 describe('Handlers', () => {
 
@@ -15,9 +15,9 @@ describe('Handlers', () => {
 
     describe('fetchWordList', () => {
         it('should return word list when axios request is successful', async () => {
-            const mockResponse = 'word1\nword2\nword3'; 
+            const mockResponse = 'word1\nword2\nword3';
 
-            axiosService.fetchData.mockResolvedValue('word1\nword2\nword3');
+            fetchData.mockResolvedValue('word1\nword2\nword3');
 
             const result = await fetchWordList({}, {});
 
@@ -29,7 +29,7 @@ describe('Handlers', () => {
 
         it('should handle axios request error', async () => {
 
-            axiosService.fetchData.mockRejectedValue(new Error('Internal Server Error'));
+            fetchData.mockRejectedValue(new Error('Internal Server Error'));
 
             const result = await fetchWordList({}, {});
 
@@ -40,7 +40,7 @@ describe('Handlers', () => {
 
     describe('fetchScores', () => {
         it('should return scores when DynamoDB scan is successful', async () => {
-            const mockItems = [{ word: 'test1', score: 10 }, { word: 'test2', score: 15 }]; 
+            const mockItems = [{ word: 'test1', score: 10 }, { word: 'test2', score: 15 }];
             AWS.mock('DynamoDB.DocumentClient', 'scan', (params, callback) => {
                 callback(null, { Items: mockItems });
             });
@@ -63,9 +63,31 @@ describe('Handlers', () => {
             expect(result.body).toContain('Internal Server Error');
         });
 
-        
+
     });
 
- 
-    
+    describe('storHighScore', () => {
+        it('should return success message when storing data is successful', async () => {
+            AWS.mock('DynamoDB.DocumentClient', 'put', (params, callback) => {
+                callback(null, {});
+            });
+            const event = {
+                body: JSON.stringify({ word: 'test1' }),
+            };
+            const result = await storeHighScore(event, {});
+            expect(result.statusCode).toBe(200);
+            expect(result.body).toContain('Item inserted successfully');
+        })
+        it('should handle DynamoDB put error', async () => {
+        
+            const event = {
+              body: JSON.stringify({ word: 'test1' }),
+            };
+        
+            const result = await storeHighScore(event, {});
+        
+            expect(result.statusCode).toBe(500);
+            expect(result.body).toContain('Error inserting item');
+          });        
+    })
 });
